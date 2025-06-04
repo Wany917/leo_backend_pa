23abecb93c21ebaf86d1d9376be438706e0805d6
Wany917 <yvannkakou03@gmail.com>
2025-06-04 23:28:38 +0200

    stockage colis

--- app/validators/create_stockage_coli.ts
+++ app/validators/create_stockage_coli.ts
@@ -3,7 +3,7 @@ import vine from '@vinejs/vine'
 export const stockageColiValidator = vine.compile(
   vine.object({
     colis_id: vine.number().positive(),
-    wharehouse_id: vine.number().positive(),
+    wharehouse_id: vine.number().optional(),
     storage_area: vine.string(),
     stored_until: vine.string(),
     description: vine.string().optional(),
