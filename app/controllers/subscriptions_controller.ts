729343d84f9e00cd5a9ed2807e09aa704137c305
LifeofLion <133486557+LifeofLion@users.noreply.github.com>
2025-06-10 14:40:31 +0200

    subscription model added with user

--- app/controllers/subscriptions_controller.ts
+++ app/controllers/subscriptions_controller.ts
@@ -1,6 +1,5 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import Subscription from '#models/subscription'
-import Utilisateurs from '#models/utilisateurs'
 import { createSubscriptionValidator, updateSubscriptionValidator } from '#validators/subscription'
 import { DateTime } from 'luxon'
 
